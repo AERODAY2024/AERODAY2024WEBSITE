d38d7697e5ac3ed2320e22909ddfe40be8c547a1
SahnounHoussem <sahnoun.houssem05@gmail.com>
2024-02-19 20:40:46 +0100

    added axes and challenges data

---
+++ src/constants/axes.js
@@ -0,0 +1,42 @@
+const aenautiques = {
+  name: "EXPOSITIONS AÉONAUTIQUES",
+  description:
+    "Des Stands éblouissent les visiteurs par un mélange de créativité et d'enthousiasme des jeunes étudiants intéressés par l'aéronautique, enrichi par le savoir-faire des expérimentés. Parmi les différents projets exposés, on cite Le simulateur de vol monoplace à 360° crée par l'équipe Technique du Club Aerobotix INSAT et exposé lors de l'édition de 2014 celui-ci ressemble au célèbre simulateur de vol de Microsoft pour le monde 3D. Une fois terminé, il sera capable de simuler l'ensemble des mouvements d'avions.",
+  responsable: "aaaa",
+};
+
+const aerospatiales = {
+  name: "EXPOSITIONS AÉROSPATIALES",
+  description:
+    "Dans un monde où les ressources diminuent inexorablement, beaucoup pensent que l’avenir de l’homme est dans l’exploitation de l’espace. Depuis son apparition, le secteur aérospatial a joué un role de pionnier et de moteur dans la détermination des origines de la vie, mais aussi dans la recherche et le développement. En Tunisie, désormais l’espace fait rêver de nombreux jeunes, qui cherchent à approfondir leurs connaissances dans ce domaine, toujours absent dans nos systèmes éducatifs. C’est pour cela que la conquête de l’espace, ainsi que l’astronomie, intègrent pour la 5éme fois « Tunisian Aeroday ». On voudrait alors permettre aux jeunes passionnés de l’astronomie de partager la richesse de ce domaine à travers plusieurs activités. L’édition 2019 du Tunisian Aeroday comporte une multitude de projets à buts variés qui offrent l’opportunité d’explorer l’espace et de se plonger dans l’inconnu . Elle englobe tout ce qui concerne la conquête spatiale, remonte d’abord le temps au tout début de l’histoire de l’univers puis permet de découvrir le présent de l’astronomie et de l’aéronautique pour se projeter après dans l’avenir de ces domaines. Parce que l'espace ne cesse de nous fasciner et de nous révéler ses plus grand secrets, Tunisian Aeroday vous fournit un espace idéal pour exposer vos projets et pour étendre votre connaissance aérospatiale.",
+  responsable: "aaaa",
+};
+
+const novices = {
+  name: "ATELIERS NOVICES",
+  description:
+    "Et parce que nous croyons en la capacité des générations à venir, on a conçu des ateliers novices qui ont pour objectif de créer un mini Aeroday pour les enfants âgés de 4 à 17 ans et leur faire surtout découvrir le monde de l'aéronautique. Et donc, des ateliers par âge, des Stands et des challenges CAO, Aéromodélisme, Gaming adaptés à leurs potentiels seront au rendez-vous...",
+  responsable: "aaaa",
+};
+
+const airshow = {
+  name: "AIRSHOW",
+  description:
+    "L'Airshow est un challenge indoor consacré aux avions radio-commandées. Lors de ce show aérien compétitif les participants sont amenés à présenter un show spectaculaire , utile et innovant qui met en valeur leurs compétences de pilotage . Un jury professionnel et un public enthousiaste seront présents pour évaluer les différentes performances.",
+  responsable: "aaaa",
+};
+
+const ambassade = {
+  name: "AMBASSADE",
+  description:
+    "L'ambassade est l'axe de l'Aeroday qui vise à l'internationalisation de l'événement soit par l'invitation des conférenciers, participants ou média internationaux.",
+  responsable: "aaaa",
+};
+
+export const axes = {
+  aenautiques,
+  aerospatiales,
+  novices,
+  airshow,
+  ambassade,
+};
