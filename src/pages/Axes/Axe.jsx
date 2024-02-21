668a4efc682e1d8baea4a9debed212f5cf5f3563
Sanji-Vin <ayoubferjani789@gmail.com>
2024-02-21 15:21:45 +0100

    added axe pages

--- src/pages/Axes/Axe.jsx
+++ src/pages/Axes/Axe.jsx
@@ -1,15 +1,40 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams} from "react-router-dom";
 import { axes } from "../../constants";
 import AxeCard from "./components/AxeCard";
-
+import "../../assets/styles/common/index.scss"
 const Axe = () => {
+  const navigate = useNavigate();
   let { axeName } = useParams();
   const currentAxe = axes[axeName];
-
+if(currentAxe== undefined) {
+  console.log("error");ù
+}
   return (
     <div className="mt-5 pt-5">
-      <h1>{currentAxe.name}</h1>
-      <AxeCard axe={currentAxe} />
+      <div className="col-lg-8 col-md-10 mx-auto">
+				<div className="block">
+					<article className="blog-post single">
+						<div className="post-thumb">
+							<img src={currentAxe.image} alt="post-image" className="img-fluid"/>
+						</div>
+						<div className="post-content">
+							<div className="date">
+								<h4>20<span>May</span></h4>
+							</div>
+							<div className="post-title">
+								<h3>{currentAxe.name}</h3>
+							</div>
+							<div className="post-meta">
+							</div>
+							<div className="post-details">
+                <p>{currentAxe.description}
+                </p>
+									
+							</div>
+						</div>
+					</article>
+				</div>
+			</div>
     </div>
   );
 };
