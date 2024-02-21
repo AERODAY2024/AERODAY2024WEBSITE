e542ed7c023eb61536d251f65d1241491fdeb7f5
Sanji-Vin <ayoubferjani789@gmail.com>
2024-02-21 15:48:15 +0100

    fixed scss

--- src/pages/Axes/Axe.jsx
+++ src/pages/Axes/Axe.jsx
@@ -1,7 +1,7 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { axes } from "../../constants";
 import AxeCard from "./components/AxeCard";
-import "../../assets/styles/common/index.scss";
+import "../../assets/styles/pages/blog.scss"
 import { useEffect } from "react";
 
 const Axe = () => {
@@ -24,26 +24,7 @@ const Axe = () => {
     <div className="mt-5 pt-5">
       <div className="col-lg-8 col-md-10 mx-auto">
 				<div className="block">
-					<article className="blog-post single">
-						<div className="post-thumb">
-							<img src={currentAxe.image} alt="post-image" className="img-fluid"/>
-						</div>
-						<div className="post-content">
-							<div className="date">
-								<h4>20<span>May</span></h4>
-							</div>
-							<div className="post-title">
-								<h3>{currentAxe.name}</h3>
-							</div>
-							<div className="post-meta">
-							</div>
-							<div className="post-details">
-                <p>{currentAxe.description}
-                </p>
-									
-							</div>
-						</div>
-					</article>
+          <AxeCard axe={currentAxe}/>
 				</div>
 			</div>
     </div>
--- src/pages/Axes/components/AxeCard.jsx
+++ src/pages/Axes/components/AxeCard.jsx
@@ -1,9 +1,25 @@
 const AxeCard = ({ axe }) => {
   return (
-    <div>
-      <h1>{axe.name}</h1>
-      <p>{axe.description}</p>
+    <article className="blog-post single">
+    <div className="post-thumb">
+      <img src={axe.image} alt="post-image" className="img-fluid"/>
     </div>
+    <div className="post-content">
+      <div className="date">
+        <h4>20<span>May</span></h4>
+      </div>
+      <div className="post-title">
+        <h3>{axe.name}</h3>
+      </div>
+      <div className="post-meta">
+      </div>
+      <div className="post-details">
+        <p>{axe.description}
+        </p>
+          
+      </div>
+    </div>
+  </article>
   );
 };
 
