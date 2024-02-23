b9a725a670e765fc49ab09077cf98a2eeda66c35
Sanji-Vin <ayoubferjani789@gmail.com>
2024-02-23 12:38:50 +0100

    Added the AxeCard component to the AxeSection

--- src/pages/Axes/components/AxeCard.jsx
+++ src/pages/Axes/components/AxeCard.jsx
@@ -1,26 +1,38 @@
 const AxeCard = ({ axe }) => {
   return (
-    <article className="blog-post single">
-    <div className="post-thumb">
-      <img src={axe.image} alt="post-image" className="img-fluid"/>
+    <div className="col-lg-4 col-md-6 col-sm-8">
+    <div className="blog-post">
+      <div className="post-thumb">
+        <a href="news-single.html">
+          <img src="images/news/post-thumb-one.jpg" alt="post-image" className="img-fluid"/>
+        </a>
+      </div>
+      <div className="post-content">
+        <div className="date">
+          <h4>20<span>May</span></h4>
+        </div>
+        <div className="post-title">
+          <h2><a href="news-single.html">Elementum purus id ultrices.</a></h2>
+        </div>
+        <div className="post-meta">
+          <ul className="list-inline">
+            <li className="list-inline-item">
+              <i className="fa fa-user-o"></i>
+              <a href="#">Admin</a>
+            </li>
+            <li className="list-inline-item">
+              <i className="fa fa-heart-o"></i>
+              <a href="#">350</a>
+            </li>
+            <li className="list-inline-item">
+              <i className="fa fa-comments-o"></i>
+              <a href="#">30</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-    <div className="post-content">
-      <div className="date">
-        <h4>20<span>May</span></h4>
-      </div>
-      <div className="post-title">
-        <h3>{axe.name}</h3>
-      </div>
-      <div className="post-meta">
-      </div>
-      <div className="post-details">
-        <p>{axe.description}
-        </p>
-          
-      </div>
-    </div>
-  </article>
-  );
-};
-
+  </div>
+  )
+}
 export default AxeCard;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import "../../assets/styles/pages/home.scss";
 import AboutSection from "../../components/AboutSection";
 import Banner from "../../components/Banner";
+import { AxesSection } from "../Axes";  
+
 const Home = () => {
   return (
     <>
       <Banner />
       <AboutSection />
+      <AxesSection/>
     </>
   );
 };
