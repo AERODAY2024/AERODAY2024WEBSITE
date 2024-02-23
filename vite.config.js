87ea12707e92e10f543a56f2696d9f98ac8f2ad0
SahnounHoussem <sahnoun.houssem05@gmail.com>
2024-02-24 00:16:57 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined, // Allow Vite to handle chunks
-        scriptType: "module", // Set script type to module
-      },
-    },
-  },
 });
