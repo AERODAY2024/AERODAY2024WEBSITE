e45513f41ce20bc4925dc3fb6fe05697031e7f19
SahnounHoussem <sahnoun.houssem05@gmail.com>
2024-02-23 17:57:34 +0100

    render axes list

--- src/pages/Axes/AxesSection.jsx
+++ src/pages/Axes/AxesSection.jsx
@@ -1,25 +1,29 @@
+import { axesArray } from "../../constants";
 import AxeCard from "../Axes/components/AxeCard";
-const renderedAxeCards = Array.from({ length: 5 }, (_, index) => (
-  <AxeCard key={index} />
-));
-const AxesSection = ()=>{
 
-    return (
-        <section className="news section">         
-  <div className="container">
-    <div className="row">
-      <div className="col-12">
-        <div className="section-title">
-          <h3>Eventre <span className="alternate">News</span></h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusm tempor incididunt ut labore dolore</p>
+const renderedAxeCards = axesArray.map((axe) => {
+  return <AxeCard key={axe.name} axe={axe} />;
+});
+const AxesSection = () => {
+  return (
+    <section className="news section">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="section-title">
+              <h3>
+                Eventre <span className="alternate">News</span>
+              </h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
+                eiusm tempor incididunt ut labore dolore
+              </p>
+            </div>
+          </div>
         </div>
+        <div className="row">{renderedAxeCards}</div>
       </div>
-    </div>
-    <div className="row">
-    {renderedAxeCards}
-    </div>
-  </div>
-</section>
-    )
-} 
-export {AxesSection}
+    </section>
+  );
+};
+export { AxesSection };
