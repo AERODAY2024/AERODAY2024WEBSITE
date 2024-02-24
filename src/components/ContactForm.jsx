9365d079c21d86821255714c089624c4abebeacd
SahnounHoussem <sahnoun.houssem05@gmail.com>
2024-02-25 00:31:45 +0100

    add our contacts to contact page

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -37,81 +37,79 @@ const ContactForm = () => {
   };
 
   return (
-    <section className="section contact-form">
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <div className="section-title">
-              <h3>
-                Contactez <span className="alternate">Nous</span>
-              </h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Maiores, velit.
-              </p>
-            </div>
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <div className="section-title">
+            <h3>
+              Contactez <span className="alternate">Nous</span>
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores,
+              velit.
+            </p>
           </div>
         </div>
-        <form className="row" onSubmit={handleContactFormSubmission}>
-          <div className="col-md-6">
-            <input
-              type="text"
-              className="form-control main"
-              id="name"
-              placeholder="Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <div className="col-md-6">
-            <input
-              type="email"
-              className="form-control main"
-              id="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="col-md-12">
-            <input
-              type="text"
-              className="form-control main"
-              id="phone"
-              placeholder="Phone"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-            />
-          </div>
-          <div className="col-md-12">
-            <textarea
-              id="message"
-              className="form-control main"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              rows={10}
-              placeholder="Your Message"
-            ></textarea>
-          </div>
-          <div className="col-12 text-center row mx-auto">
-            <button
-              type="submit"
-              className="btn btn-main-md  mx-auto col-md-5"
-              disabled={isLoading || !name || !email || !phone || !message}
-            >
-              Send Message
-            </button>
-            <button
-              type="button"
-              className="btn btn-main-md mx-auto col-md-5 "
-              onClick={clearForm}
-            >
-              Clear Message
-            </button>
-          </div>
-        </form>
       </div>
-    </section>
+      <form className="row" onSubmit={handleContactFormSubmission}>
+        <div className="col-md-6">
+          <input
+            type="text"
+            className="form-control main"
+            id="name"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="col-md-6">
+          <input
+            type="email"
+            className="form-control main"
+            id="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="col-md-12">
+          <input
+            type="text"
+            className="form-control main"
+            id="phone"
+            placeholder="Phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </div>
+        <div className="col-md-12">
+          <textarea
+            id="message"
+            className="form-control main"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            rows={10}
+            placeholder="Your Message"
+          ></textarea>
+        </div>
+        <div className="col-12 text-center row mx-auto">
+          <button
+            type="submit"
+            className="btn btn-main-md  mx-auto col-md-5"
+            disabled={isLoading || !name || !email || !phone || !message}
+          >
+            Send Message
+          </button>
+          <button
+            type="button"
+            className="btn btn-main-md mx-auto col-md-5 "
+            onClick={clearForm}
+          >
+            Clear Message
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,11 +1,13 @@
 import "../../assets/styles/pages/contact.scss";
 import { ContactForm } from "../../components";
+import { OurContacts } from "./component";
 
 const Contact = () => {
   return (
-    <div>
+    <section className="section contact-form">
+      <OurContacts />
       <ContactForm />
-    </div>
+    </section>
   );
 };
 
